9e3d44b1d616047ffa8d542c975a0d3d3bea7b88
bc01012 <67948566+bc01012@users.noreply.github.com>
2020-12-17 14:43:46 +0800

    Create webcrawpkg.go

---
+++ webcrawpkg/webcrawpkg.go
@@ -0,0 +1,45 @@
+package webcrawpkg
+
+import (
+	"log"
+	"net/http"
+	"os"
+	// "github.com/ofabry/go-callvis/examples/main/webcrawpkg/job"
+)
+
+var l = log.New(os.Stderr, "mypkg", 0)
+
+var T = new(WebSpider)
+
+type WebSpider struct{}
+
+func init() {
+	go func() {
+		l.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+}
+
+func NewWebSpider() *WebSpider {
+	return &WebSpider{}
+}
+
+func (t *WebSpider) Identifier() {}
+func (t *WebSpider) StartTask() {}
+func (t *WebSpider) ScrapData() {}
+func (t *WebSpider) PraseData() {}
+func (t *WebSpider) WriteInDB() {}
+func (t *WebSpider) SendEmail() {}
+
+func RunTask() {
+	defer deferred()
+	go Concurrent(T)
+	T.StartTask()
+}
+func deferred() {}
+
+func Concurrent(T *WebSpider) {
+	T.ScrapData()
+	T.PraseData()
+	T.WriteInDB()
+	T.SendEmail()
+}
